1df0af60d983217f1b284a93a5f9fd4b0312761c
TouchToFly <touchtofly@Jons-MacBook-Pro-2.local>
2020-04-20 15:43:37 -0700

    Refactor

---
+++ Codegen/Sources/Codegen/main.swift
@@ -0,0 +1,36 @@
+import ApolloCodegenLib
+import FileProvider
+
+let parentFolderOfScriptFile = FileFinder.findParentFolder()
+
+//This is the directory that the entire project lives in.
+let projectDirectory = parentFolderOfScriptFile.deletingLastPathComponent().deletingLastPathComponent().deletingLastPathComponent()
+
+//This is the directory for the Codegen
+let codegenDirectory = projectDirectory.appendingPathComponent("Codegen")
+
+//This will tack on a directory in the Codegen folder called "ApolloCLI". This is where the Apollo Node tools get downloaded and stored. I have told my .gitignore file to ignore this folder.
+let cliFolderURL = codegenDirectory.appendingPathComponent("ApolloCLI")
+
+//This is the folder in my Xcode project that I want the schema.json file to be downloaded and the API.swift file to be generated. All my <file>.graphql files are here.
+let graphQLFolder = projectDirectory.appendingPathComponent("GraphQLSwiftUI").appendingPathComponent("GraphQL")
+
+let yelpGraphQLURL = URL(string: "https://api.yelp.com/v3/graphql")!
+
+let yelpAPIKey: String = "qco6Jeyqwo8-v8QRaCxWSiO5Fwr7JhWOSObUthK4ajdFVGuTvPHNjmi_b9A_UaOAmJzo2Vt6GlEmyxJwIOs2yWXr3FcPK0x3QBA-YpLFPq-w_UnIl9Q4bb0_SumwXHYx"
+
+let schemaDownloadOptions: ApolloSchemaOptions = ApolloSchemaOptions.init(endpointURL: yelpGraphQLURL, header: "Authorization: Bearer \(yelpAPIKey)", outputFolderURL: graphQLFolder)
+
+let codeGenerationOption: ApolloCodegenOptions = ApolloCodegenOptions.init(targetRootURL: graphQLFolder)
+
+do {
+    
+    //Do the schema download
+    try ApolloSchemaDownloader.run(with: cliFolderURL, options: schemaDownloadOptions)
+    
+    try ApolloCodegen.run(from: graphQLFolder, with: cliFolderURL, options: codeGenerationOption)
+    
+}catch{
+    print(error)
+    exit(1)
+}
---
+++ GraphQLSwiftUI/GraphQL/API.swift
@@ -0,0 +1,203 @@
+// @generated
+//  This file was automatically generated and should not be edited.
+
+import Apollo
+import Foundation
+
+public final class BusinessesQuery: GraphQLQuery {
+  /// The raw GraphQL definition of this operation.
+  public let operationDefinition: String =
+    """
+    query Businesses($searchTerm: String, $latitude: Float, $longitude: Float) {
+      search(term: $searchTerm, latitude: $latitude, longitude: $longitude) {
+        __typename
+        business {
+          __typename
+          id
+          name
+          url
+          rating
+          photos
+          distance
+        }
+      }
+    }
+    """
+
+  public let operationName: String = "Businesses"
+
+  public let operationIdentifier: String? = "f58ba3f3222ffac342294620663307745619728b02d84f801a22997c200f631b"
+
+  public var searchTerm: String?
+  public var latitude: Double?
+  public var longitude: Double?
+
+  public init(searchTerm: String? = nil, latitude: Double? = nil, longitude: Double? = nil) {
+    self.searchTerm = searchTerm
+    self.latitude = latitude
+    self.longitude = longitude
+  }
+
+  public var variables: GraphQLMap? {
+    return ["searchTerm": searchTerm, "latitude": latitude, "longitude": longitude]
+  }
+
+  public struct Data: GraphQLSelectionSet {
+    public static let possibleTypes: [String] = ["Query"]
+
+    public static let selections: [GraphQLSelection] = [
+      GraphQLField("search", arguments: ["term": GraphQLVariable("searchTerm"), "latitude": GraphQLVariable("latitude"), "longitude": GraphQLVariable("longitude")], type: .object(Search.selections)),
+    ]
+
+    public private(set) var resultMap: ResultMap
+
+    public init(unsafeResultMap: ResultMap) {
+      self.resultMap = unsafeResultMap
+    }
+
+    public init(search: Search? = nil) {
+      self.init(unsafeResultMap: ["__typename": "Query", "search": search.flatMap { (value: Search) -> ResultMap in value.resultMap }])
+    }
+
+    /// Search for businesses on Yelp.
+    public var search: Search? {
+      get {
+        return (resultMap["search"] as? ResultMap).flatMap { Search(unsafeResultMap: $0) }
+      }
+      set {
+        resultMap.updateValue(newValue?.resultMap, forKey: "search")
+      }
+    }
+
+    public struct Search: GraphQLSelectionSet {
+      public static let possibleTypes: [String] = ["Businesses"]
+
+      public static let selections: [GraphQLSelection] = [
+        GraphQLField("__typename", type: .nonNull(.scalar(String.self))),
+        GraphQLField("business", type: .list(.object(Business.selections))),
+      ]
+
+      public private(set) var resultMap: ResultMap
+
+      public init(unsafeResultMap: ResultMap) {
+        self.resultMap = unsafeResultMap
+      }
+
+      public init(business: [Business?]? = nil) {
+        self.init(unsafeResultMap: ["__typename": "Businesses", "business": business.flatMap { (value: [Business?]) -> [ResultMap?] in value.map { (value: Business?) -> ResultMap? in value.flatMap { (value: Business) -> ResultMap in value.resultMap } } }])
+      }
+
+      public var __typename: String {
+        get {
+          return resultMap["__typename"]! as! String
+        }
+        set {
+          resultMap.updateValue(newValue, forKey: "__typename")
+        }
+      }
+
+      /// A list of business Yelp finds based on the search criteria.
+      public var business: [Business?]? {
+        get {
+          return (resultMap["business"] as? [ResultMap?]).flatMap { (value: [ResultMap?]) -> [Business?] in value.map { (value: ResultMap?) -> Business? in value.flatMap { (value: ResultMap) -> Business in Business(unsafeResultMap: value) } } }
+        }
+        set {
+          resultMap.updateValue(newValue.flatMap { (value: [Business?]) -> [ResultMap?] in value.map { (value: Business?) -> ResultMap? in value.flatMap { (value: Business) -> ResultMap in value.resultMap } } }, forKey: "business")
+        }
+      }
+
+      public struct Business: GraphQLSelectionSet {
+        public static let possibleTypes: [String] = ["Business"]
+
+        public static let selections: [GraphQLSelection] = [
+          GraphQLField("__typename", type: .nonNull(.scalar(String.self))),
+          GraphQLField("id", type: .scalar(String.self)),
+          GraphQLField("name", type: .scalar(String.self)),
+          GraphQLField("url", type: .scalar(String.self)),
+          GraphQLField("rating", type: .scalar(Double.self)),
+          GraphQLField("photos", type: .list(.scalar(String.self))),
+          GraphQLField("distance", type: .scalar(Double.self)),
+        ]
+
+        public private(set) var resultMap: ResultMap
+
+        public init(unsafeResultMap: ResultMap) {
+          self.resultMap = unsafeResultMap
+        }
+
+        public init(id: String? = nil, name: String? = nil, url: String? = nil, rating: Double? = nil, photos: [String?]? = nil, distance: Double? = nil) {
+          self.init(unsafeResultMap: ["__typename": "Business", "id": id, "name": name, "url": url, "rating": rating, "photos": photos, "distance": distance])
+        }
+
+        public var __typename: String {
+          get {
+            return resultMap["__typename"]! as! String
+          }
+          set {
+            resultMap.updateValue(newValue, forKey: "__typename")
+          }
+        }
+
+        /// Yelp ID of this business.
+        public var id: String? {
+          get {
+            return resultMap["id"] as? String
+          }
+          set {
+            resultMap.updateValue(newValue, forKey: "id")
+          }
+        }
+
+        /// Name of this business.
+        public var name: String? {
+          get {
+            return resultMap["name"] as? String
+          }
+          set {
+            resultMap.updateValue(newValue, forKey: "name")
+          }
+        }
+
+        /// URL for business page on Yelp.
+        public var url: String? {
+          get {
+            return resultMap["url"] as? String
+          }
+          set {
+            resultMap.updateValue(newValue, forKey: "url")
+          }
+        }
+
+        /// Rating for this business (value ranges from 1, 1.5, ... 4.5, 5).
+        public var rating: Double? {
+          get {
+            return resultMap["rating"] as? Double
+          }
+          set {
+            resultMap.updateValue(newValue, forKey: "rating")
+          }
+        }
+
+        /// URLs of up to three photos of the business.
+        public var photos: [String?]? {
+          get {
+            return resultMap["photos"] as? [String?]
+          }
+          set {
+            resultMap.updateValue(newValue, forKey: "photos")
+          }
+        }
+
+        /// When searching, this provides the distance of the business from the search location in meters
+        public var distance: Double? {
+          get {
+            return resultMap["distance"] as? Double
+          }
+          set {
+            resultMap.updateValue(newValue, forKey: "distance")
+          }
+        }
+      }
+    }
+  }
+}
