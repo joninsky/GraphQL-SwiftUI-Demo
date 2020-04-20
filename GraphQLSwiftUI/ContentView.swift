c81394be1bd856879a5c52adbb6be28d93476b35
TouchToFly <touchtofly@Jons-MacBook-Pro-2.local>
2020-04-20 11:34:40 -0700

    First Commit

---
+++ GraphQLSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GraphQLSwiftUI
+//
+//  Created by TouchToFly on 4/20/20.
+//  Copyright © 2020 TouchToFly. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
